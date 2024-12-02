32da2cef7da9c60b0cd25489340a4abc34baa8ca
Olesia <80747385+Olesya002@users.noreply.github.com>
2024-12-02 20:09:04 +0300

    Update memory_card_stepan.py

--- Python_2/memory_card_stepan.py
+++ Python_2/memory_card_stepan.py
@@ -15,21 +15,21 @@ def start_test():
     Randobox.hide()
     unRandobox.show()
     button.setText('следующий тест')
-def ask(q,qesthon):                               ''' тут оставить просто q '''
+def ask(q,qesthon):                                               ''' тут оставить просто q '''
     shuffle(answer)
     answer[0].setText(q.right_answer)
     answer[1].setText(q.wrong1)
     answer[2].setText(q.wrong2)
     answer[3].setText(q.wrong3)
     inscriptions1.setText(q.qesthon)
-    inscriptions1.setText(q.right_answer)
+    inscriptions1.setText(q.right_answer)                         ''' тут следует записать верный ответ в нужный виджет (у тебя это rc1) '''
 class Qesthon:
     def __init__(self,qesthon,right_answer,wrong1,wrong2,wrong3):
         self.qesthon = qesthon
         self.right_answer = right_answer
         self.wrong1 = wrong1
         self.wrong2 = wrong2
-        self.wrong3 = wrong2                      ''' тут self.wrong3 = wrong3 '''
+        self.wrong3 = wrong2                                      ''' тут self.wrong3 = wrong3 '''
     
 qesthon1 = []
 qesthon1.append(Qesthon('Самое глубокое озеро в Мире?','байкал','	Танганьика','Восток','Ладожское озеро'))
@@ -39,20 +39,20 @@ app = QApplication([])
 window = QWidget()
 
 lind = QVBoxLayout()
-inscriptions1 = QLabel('тест')
+inscriptions1 = QLabel('тест')                                                       ''' это надпись для вопроса (именно в нее мы записываем новый вопрос в функции ask) '''
 lind.addWidget(inscriptions1)
 
-Randobox = QGroupBox('какая комбинация клафишь копирует выделенный текс?')           ''' этот вопрос можешь тоже добавить в список qesthon1 '''
+Randobox = QGroupBox('какая комбинация клафишь копирует выделенный текс?')           ''' тут ты создаешь форму (то, что в скобках - это заголовок для формы с вариантами ответа, можно написать "Варианты") '''
 r1 = QRadioButton('ctrl + c')
 r2 = QRadioButton('ctrl + v')
 r3 = QRadioButton('ctrl + a')
 r4 = QRadioButton('crtl + s')
 answer = [r1,r2,r3,r4]
-v = QVBoxLayout()                                                                   '''линия v у тебя нигде не используется, убери ее вообще'''
-unRandobox = QGroupBox('какая комбинация клафишь копирует выделенный текст?')
-rc1 = QLabel('Правильный ответ \n crtl + c')                                           ''' эта надпись нигде не используется, убери ее '''
-v.addWidget(r1)              ''' убрать '''
-v.addWidget(r2)              ''' убрать '''
+v = QVBoxLayout()                                                                    ''' линия v пусть будет для формы unRandobox '''
+unRandobox = QGroupBox('какая комбинация клафишь копирует выделенный текст?')        ''' в скобках заголовок для формы - можно написать "Результат" '''
+rc1 = QLabel('Правильный ответ \n crtl + c')                                         ''' это надпись для верного ответа (она должна быть в форме unRandobox и именно в нее мы записываем правильный ответ в ф-ии ask). '''
+v.addWidget(r1)                                                                      ''' закрепи rc1 на v: v.addWidget(rc1) + затем закрепи эту линию в группу: unRandobox.setLayout(v) ''' 
+v.addWidget(r2)              ''' убрать '''                                            
 v.addWidget(r3)              ''' убрать '''
 v.addWidget(r4)              ''' убрать '''
 layout1 = QVBoxLayout()
