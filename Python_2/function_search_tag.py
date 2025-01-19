3ac43636292fab2c1aff6c7dfb91635fae36f089
Olesia <80747385+Olesya002@users.noreply.github.com>
2025-01-19 14:41:12 +0300

    Create function_search_tag.py

---
+++ Python_2/function_search_tag.py
@@ -0,0 +1,23 @@
+def search_tag():
+    print(button_tag_search.text())
+    tag = field_tag.text()
+    if button_tag_search.text() == "Искать заметки по тегу" and tag:
+        print(tag)
+        notes_filtered = {} #тут будут заметки с выделенным тегом
+        for note in notes:
+            if tag in notes[note]["теги"]: 
+                notes_filtered[note]=notes[note]
+        button_tag_search.setText("Сбросить поиск")
+        list_notes.clear()
+        list_tags.clear()
+        list_notes.addItems(notes_filtered)
+        print(button_tag_search.text())
+    elif button_tag_search.text() == "Сбросить поиск":
+        field_tag.clear()
+        list_notes.clear()
+        list_tags.clear()
+        list_notes.addItems(notes)
+        button_tag_search.setText("Искать заметки по тегу")
+        print(button_tag_search.text())
+    else:
+        pass
