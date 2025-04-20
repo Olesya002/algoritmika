35a8af807e03fa11370d60fda80a6c7a6b2ef17f
Olesia <80747385+Olesya002@users.noreply.github.com>
2025-04-20 10:50:25 +0300

    Update game.py

--- panda3d/game.py
+++ panda3d/game.py
@@ -1,16 +1,18 @@
+# напиши здесь код основного окна игры
 from direct.showbase.ShowBase import ShowBase
 from mapmanager import Mapmanager
 from hero import Hero
-
 class Game(ShowBase):
-   def __init__(self):
-       ShowBase.__init__(self)
-       self.land = Mapmanager()
-       self.land.loadLand("land.txt")
-       self.hero = Hero((10,10,10),self.land)
-       base.camLens.setFov(90)
-       
+    def __init__(self):
+        ShowBase.__init__(self)
+        self.land = Mapmanager()
+        base.camLens.setFov(90)
+        self.land.loadLand('land.txt')
+        # self.land.loadRandomMap(25, 25, 0, 8)
+        self.hero = Hero((2, 2, 2), self.land)
 
 
+
+        
 game = Game()
 game.run()
